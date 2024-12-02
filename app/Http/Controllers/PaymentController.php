efdbe907dfca919f6be53e45653c17d5baa2b869
Gonzalo Cárdenas <lalo020404@gmail.com>
2024-12-01 19:25:22 -0600

    Se actualizan Payments

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -16,6 +16,8 @@ class PaymentController extends Controller
     public function index()
     {
         //
+        $payments = Payment::all();
+        return view('payments.index', compact('payments'));
     }
 
     /**
