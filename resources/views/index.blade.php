414da46282ddfa0bbc077821bab0afa50d90db38
Gonzalo Cárdenas <lalo020404@gmail.com>
2024-12-02 21:49:25 -0600

    Se modifica el index principal

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,14 +3,14 @@
 @section('content')
 <div class="container mt-5">
     <div class="jumbotron">
-        <h1 class="display-4">Welcome to PayFlow!</h1>
-        <p class="lead">The best platform to manage your financial transactions.</p>
-        <p>Here are some quick links to get you started:</p>
+        <h1 class="display-4">Bienvenido a PayFlow!</h1>
+        <p class="lead">La mejor plataforma para gestionar tus proyectos y transacciones.</p>
+        <p>Enlaces de inicio rápido:</p>
         <ul class="list-group">
-            <li class="list-group-item"><a href="#">View Clients</a></li>
-            <li class="list-group-item"><a href="#">View Suppliers</a></li>
-            <li class="list-group-item"><a href="#">Make a Payment</a></li>
-            <li class="list-group-item"><a href="#">Request an Advance</a></li>
+            <li class="list-group-item"><a href="{{ route('client.index') }}">Clientes</a></li>
+            <li class="list-group-item"><a href="{{ route('supplier.index') }}">Proveedores</a></li>
+            <li class="list-group-item"><a href="{{ route('payment.index') }}">Pagos</a></li>
+            <li class="list-group-item"><a href="{{ route('advance.index') }}">Adelantos</a></li>
         </ul>
     </div>
 </div>
